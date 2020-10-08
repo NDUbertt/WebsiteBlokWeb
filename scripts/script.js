eacbfe1fcf4ff413381d2cdf03ea9905e0dbd40d
NDUbertt <65679919+NDUbertt@users.noreply.github.com>
2020-10-08 20:28:50 +0200

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,22 @@
 // JavaScript Document
+var heartButton = document.querySelector('.heartbutton')
+
+function toonHeart() {
+  document.getElementsByClassName("heartplaatje")[0].src = "images/heartfull.svg"
+}
+
+if (heartButton) {
+  heartButton.addEventListener('click', toonHeart);
+}
+//footer
+var footer = document.querySelectorAll("footer section > button");
+console.log(footer)
+
+// function footerTonen() {
+//   document.body.classList.toggle("toonFooter");
+// }
+footer.forEach(footerButton => {
+  footerButton.addEventListener("click", (event) => {
+    event.target.classList.toggle('toonFooter')
+  })
+})
