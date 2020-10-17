d7fda903c98497d58f037514c14ebb8b3a712623
NDUbertt <65679919+NDUbertt@users.noreply.github.com>
2020-10-17 12:45:19 +0200

    Update and rename scripts/script.js to javascript.js

--- javascript.js
+++ javascript.js
@@ -1,13 +1,15 @@
-// JavaScript Document
-var heartButton = document.querySelector('.heartbutton')
 
+//HeartButton
+var heartButton = document.querySelector('.heartbutton')
+ 
 function toonHeart() {
   document.getElementsByClassName("heartplaatje")[0].src = "images/heartfull.svg"
 }
-
+ 
 if (heartButton) {
   heartButton.addEventListener('click', toonHeart);
 }
+  
 //footer
 var footer = document.querySelectorAll("footer section > button");
 console.log(footer)
@@ -20,3 +22,17 @@ footer.forEach(footerButton => {
     event.target.classList.toggle('toonFooter')
   })
 })
+
+//hamburgermenu
+var hamburger = document.querySelector('nav ul:nth-of-type(2) li button');
+console.log(hamburger)
+var hamburgerClose = document.querySelector('nav ul:first-of-type li:last-of-type button');
+
+console.log(hamburgerClose)
+
+function toonHamburger() {
+    document.body.classList.toggle('toonHamburger')
+}
+
+hamburger.addEventListener('click', toonHamburger);
+hamburgerClose.addEventListener('click', toonHamburger)
